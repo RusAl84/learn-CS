f48d2a72bfe59e7713b9545cafe4852360d5123c
Alexey Rusakov <rusal@bk.ru>
2021-10-12 00:24:20 +0300

    Добавьте файлы проекта.

---
+++ Classes/EzhClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace learnCSClasses { 
+
+public class EzhClass
+	{
+		public string name;
+	  public int age;
+	  public  double health;
+	  public void Display()
+		{
+			Console.WriteLine($"Имя ёжика: {name}, возраст {age}");
+			
+		
+		}
+}
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft;
+using learnCSClasses;
+
+namespace learn_CS
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //Console.WriteLine("Hello World!");
+      //Console.Beep();
+      //int a = 5;
+      //Console.WriteLine(a);
+      EzhClass ezhik = new EzhClass();
+      ezhik.name = "Anton";
+      ezhik.age = 17;
+      ezhik.Display();
+    
+    }
+
+  }
+}
